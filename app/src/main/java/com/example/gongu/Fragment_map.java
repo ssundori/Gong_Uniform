aefcb7a54ba775c194528ee6471add9e8699a46b
sunwoo <b_bomb030@naver.com>
2024-02-19 17:17:12 +0900

    0219 지도페이지 권한 설정(미완성) yanzhenjie 라이브러리 관련,,

--- app/src/main/java/com/example/gongu/Fragment_map.java
+++ app/src/main/java/com/example/gongu/Fragment_map.java
@@ -1,16 +1,97 @@
 package com.example.gongu;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
 public class Fragment_map extends Fragment {
+    TextView textView;
+    Button button;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        view = inflater.inflate(R.layout.fragment_map, container, false);
+        textView = view.findViewById(R.id.textView);
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startLocationService();
+            }
+        });
         return inflater.inflate(R.layout.fragment_map, container, false);
+
+        /*AndPermission.with(this)
+                .runtime()
+                .permission(
+                        Permission.ACCESS_FINE_LOCATION,
+                        Permission.ACCESS_COARSE_LOCATION)
+                .onGranted(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> permissions) {
+                        showToast("허용된 권한 개수 : " + permissions.size());
+                    }
+                })
+                .onDenied(new Action<List<String>>() {
+                    @Override
+                    public void onAction(List<String> permissions) {
+                        showToast("거부된 권한 개수 : " + permissions.size());
+                    }
+                })
+                .start();*/
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void startLocationService() {
+        LocationManager manager= (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                String message = "최근 위치 -> Latitude : " + latitude + "\nLongitude : " + longitude;
+
+                textView.setText(message);
+            }
+
+            GPSListener gpsListener = new GPSListener();
+            long minTime = 10000; //최소 시간 10초, 10초마다 위치 정보 전달받음
+            float minDistance = 0; //최소 거리 설정
+
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, gpsListener);
+            Toast.makeText(getActivity().getApplicationContext(), "내 위치확인 요청함", Toast.LENGTH_SHORT).show();
+        } catch(SecurityException e) {
+            e.printStackTrace();
+        }
+    }
+
+    class GPSListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            Double latitude = location.getLatitude();
+            Double longitude = location.getLongitude();
+            String message = "내 위치 -> Latitude : " + latitude + "\nLongitude : " + longitude;
+            textView.setText(message);
+        }
+
+        public void onProviderDisabled(String provider) { }
+        public void onProviderEnabled(String provider) { }
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
     }
 }
