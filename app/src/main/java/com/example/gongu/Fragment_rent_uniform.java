d6db26b45dcffb153f1706d41961187dc9b3b10a
Kwon Gahyeon <bambooplw@gmail.com>
2024-03-13 17:17:43 +0900

    대여 화면 UI 1차

--- app/src/main/java/com/example/gongu/Fragment_rent_uniform.java
+++ app/src/main/java/com/example/gongu/Fragment_rent_uniform.java
@@ -8,27 +8,25 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
-public class Fragment_rent extends Fragment {
+public class Fragment_rent_uniform extends Fragment {
 
     private View view;
-    private Button RtoRUButton;
+    private Button RUtoRS;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_rent, container, false);
-        RtoRUButton = view.findViewById(R.id.rtoru);
-        RtoRUButton.setOnClickListener(new View.OnClickListener() {
+        view = inflater.inflate(R.layout.fragment_rent_uniform, container, false);
+        RUtoRS = view.findViewById(R.id.rutors);
+        RUtoRS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                Fragment_rent_uniform fragmentRentUniform = new Fragment_rent_uniform();
+                Fragment_rent_size fragmentRentSize = new Fragment_rent_size();
 
-                transaction.replace(R.id.container, fragmentRentUniform);
+                transaction.replace(R.id.container, fragmentRentSize);
 
                 transaction.commit();
             }
         });
         return view;
     }
-
 }
