2209397a3cd5bd48d4ce850370c435fbdd5153ea
sunwoo <b_bomb030@naver.com>
2024-03-13 17:35:53 +0900

    0313 지도페이지 지점 리스트 등록(미완성)

--- app/src/main/java/com/example/gongu/Fragment_map.java
+++ app/src/main/java/com/example/gongu/Fragment_map.java
@@ -1,15 +1,12 @@
 package com.example.gongu;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,12 +16,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -75,7 +69,7 @@ public class Fragment_map extends Fragment implements OnMapReadyCallback {
     @Override
     public void onStart() {
         super.onStart();
-        }
+    }
 
     private void showToast(String message) {
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show();
