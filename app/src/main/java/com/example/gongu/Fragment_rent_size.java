67ecd74387bc42ecdacfc8ea9d61047d9e7da4f2
sunwoo <b_bomb030@naver.com>
2024-04-08 12:10:33 +0900

    0408 QR발급 (미완성 - 버튼 클릭시 생성하는 이벤트)

--- app/src/main/java/com/example/gongu/Fragment_rent_size.java
+++ app/src/main/java/com/example/gongu/Fragment_rent_size.java
@@ -1,12 +1,20 @@
 package com.example.gongu;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +22,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Fragment_rent_size extends Fragment {
+    //private SharedViewModel QRViewModel;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -53,7 +62,46 @@ public class Fragment_rent_size extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        //QRViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
     }
+
+    public void generateQRCode(ImageView qrView) {
+        QRCodeWriter qrCode = new QRCodeWriter();
+        try {
+            // QR코드 생성 및 BitMatrix로 변환
+            BitMatrix bitMtx = qrCode.encode(
+                    getActivity().getIntent().getStringExtra("id"),
+                    BarcodeFormat.QR_CODE, 350, 350
+            );
+            //BitMatrix를 Bitmap으로 변환하여 QR코드 이미지 생성
+            Bitmap bitmap = Bitmap.createBitmap(bitMtx.getWidth(),bitMtx.getHeight(), Bitmap.Config.RGB_565);
+            for (int i=0; i<bitMtx.getWidth(); i++) {
+                for (int j=0; j<bitMtx.getHeight(); j++) {
+                    int color;
+                    if (bitMtx.get(i,j)) {
+                        color = Color.BLACK;
+                    } else {
+                        color = Color.WHITE;
+                    }
+                    bitmap.setPixel(i,j,color);
+                }
+            }
+            qrView.setImageBitmap(bitmap); // QR 코드 이미지를 ImageView에 설정하여 화면에 표시
+            } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+    }
+    Button generateQRButton = getView().findViewById(R.id.button_temp_qr);
+    ImageView qrImageView = getView().findViewById(R.id.QRImageView);
+
+    //임시 qr 생성 버튼 클릭시 QR 생성
+    /*generateQRButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            generateQRCode(qrImageView);
+        }
+    });*/
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
