1e0be2949b0287211a3d73fd8341116a6bab748b
sunwoo <b_bomb030@naver.com>
2024-04-19 13:30:52 +0900

    0419 주문시각에 현재시간 뜨도록 변경

--- app/src/main/java/com/example/gongu/Fragment_QR.java
+++ app/src/main/java/com/example/gongu/Fragment_QR.java
@@ -1,8 +1,5 @@
 package com.example.gongu;
 
-import static android.app.Activity.RESULT_OK;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +9,11 @@ import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class Fragment_QR extends Fragment {
 
     public Fragment_QR() {
@@ -20,12 +22,24 @@ public class Fragment_QR extends Fragment {
 
     private View view;
     private ImageButton button_qr_QR1;
+    TextView txt_date_qr;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_qr, container, false);
         button_qr_QR1 = view.findViewById(R.id.button_qr_QR1);
+
+        // 현재 시간 가져오기
+        Date now = new Date();
+
+        // 출력 포맷 정의
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분", Locale.getDefault());
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
+        // 현재 시간을 텍스트뷰에 설정
+        txt_date_qr = view.findViewById(R.id.txt_date_qr);
+        txt_date_qr.setText("주문 시각: \n" + sdf.format(now));
 
         // 클릭 이벤트 핸들러 등록
         button_qr_QR1.setOnClickListener(new View.OnClickListener() {
